3698b856f1633dee581bffd4aa83f973cc9d97b0
jiaqi <2468260248@qq.com>
2021-06-23 19:05:34 +0800

    🎨 加了几句没啥用的代码；

--- src/otherPages/activity/MessageBoard.vue
+++ src/otherPages/activity/MessageBoard.vue
@@ -1,5 +1,8 @@
 <template>
-  <view class="message-board"> </view>
+  <view class="message-board">
+    <!-- <view class="title"> 留言墙 </view> -->
+    <input class="input-box" placeholder="说说你的想法吧..." />
+  </view>
 </template>
 
 <script>
@@ -16,5 +19,22 @@ export default {
 <style lang="scss" scoped>
 .message-board {
   min-height: 300rpx;
+  z-index: 10;
+  position: relative;
+  .title {
+    min-height: 80rpx;
+  }
+  .input-box {
+    width: 80%;
+    margin: 40rpx auto 20rpx;
+    border-radius: 50rpx;
+    height: 80rpx;
+    border: 2px solid #911904;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #ccc;
+    background-color: rgba($color: #911904, $alpha: 0.7);
+  }
 }
 </style>
