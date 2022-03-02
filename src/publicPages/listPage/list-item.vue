468644b73683d3cff28c43914d367b44547accca
jiaqi <2468260248@qq.com>
2022-03-02 19:10:35 +0800

    - 百想首页，查询店铺信息接口改为`srvhealth_store_cus_niming_detail_select` - 软件行业协会小程序配置 - 百想首页小程序增加单店铺配置逻辑 - 修复商品详情页图片间隙

--- src/publicPages/listPage/list-item.vue
+++
@@ -1,8 +0,0 @@
-<!--
- * @Author: your name
- * @Date: 2021-06-28 09:19:05
- * @LastEditTime: 2021-06-28 09:19:05
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \bx-home-cli\src\publicPages\listPage\list-item.vue
--->
