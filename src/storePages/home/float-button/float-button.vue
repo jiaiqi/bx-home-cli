b5256da67746c479c74f4d7f8b84f0c577430224
jiaqi <2468260248@qq.com>
2022-07-20 10:06:28 +0800

    首页悬浮按钮可拖拽移动

--- src/storePages/home/float-button/float-button.vue
+++ src/storePages/home/float-button/float-button.vue
@@ -1,11 +1,27 @@
 <template>
-  <view class="float-button" :style="[setStyle]" @click="toPages">
 
+  <view class="float-button-view">
+    <movable-area class="movable-area">
+      <movable-view direction="all" :x="x" :y="y" :out-of-bounds="true" @change="onChange">
+        <view class="float-button" :style="[setStyle]" @click="toPages">
+        </view>
+      </movable-view>
+    </movable-area>
   </view>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        x: 370,
+        y: 370,
+        old: {
+          x: 0,
+          y: 0
+        }
+      }
+    },
     props: {
       pageItem: {
         type: Object
@@ -25,16 +41,20 @@
       }
     },
     methods: {
+      onChange: function(e) {
+        this.old.x = e.detail.x
+        this.old.y = e.detail.y
+      },
       toPages() {
-        let url  = this.pageItem?.float_btn_url;
+        let url = this.pageItem?.float_btn_url;
         let mainData = {
-          userInfo:this.userInfo,
-          storeInfo:this.storeInfo,
-          storeUser:this.vstoreUser,
+          userInfo: this.userInfo,
+          storeInfo: this.storeInfo,
+          storeUser: this.vstoreUser,
         }
-        if(url){
+        if (url) {
           // url = this.renderStr(url,mainData) 
-          url = url.renderStr(mainData) 
+          url = url.renderStr(mainData)
           uni.navigateTo({
             url
           })
@@ -45,13 +65,37 @@
 </script>
 
 <style scoped lang="scss">
+  movable-view {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 70px;
+    width: 70px;
+    color: #fff;
+    pointer-events: auto;
+  }
+
+  movable-area {
+    height: 100rpx;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+  }
+
+  .float-button-view {
+    position: fixed;
+    bottom: 100px;
+    right: 0;
+    padding-right: 10px;
+    width: 100vw;
+    height: 50vh;
+    pointer-events: none;
+  }
+
   .float-button {
     width: 50px;
     height: 50px;
     border-radius: 50%;
-    position: fixed;
-    bottom: 150px;
-    right: 10px;
     background-repeat: no-repeat;
     background-size: 100% 100%;
     z-index: 10;
